edbac535274193a653d9852508ad5c4e76569e00
Alexandre S. Marczynski <104026889+AlexandreMarcz@users.noreply.github.com>
2022-05-02 16:55:17 -0300

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,29 @@
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from gratitude.feeds import LatestPostsFeed
+from . import views
+
+
+app_name = 'gratitude'
+
+urlpatterns = [
+    # post views
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:year>/<slug:post>/',views.post_detail,name='post_detail'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
+
+    
+] # + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+    #path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+    #    views.post_detail,
+    #    name='post_detail'),
